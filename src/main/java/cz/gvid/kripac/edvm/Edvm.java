40a472f772293042c176eb73ea82db0a4609a9da
TENMAJKL <tenmajkl@liskyjidlokod.fun>
2024-04-09 20:22:13 +0200

    initializes java files

---
+++ src/main/java/cz/gvid/kripac/edvm/Edvm.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cz.gvid.kripac.edvm;
+
+/**
+ *
+ * @author kripac 
+ */
+public class Edvm {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
